2aab9440522431314fc1d742ed3f03d636fe35f7
Yumi Studio <thanhduyls97@gmail.com>
2025-12-13 16:51:39 +0700

    Availability to check app working without backend connection

--- src/app/context/AppContext.tsx
+++ src/app/context/AppContext.tsx
@@ -1,13 +1,15 @@
 "use client"
 
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Bounce, toast } from "react-toastify";
 
 const AppContext = createContext<{
+	isOffline: boolean;
 	alertInDevelop: () => void;
 } | null>(null);
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+	const [isOffline, setIsOffline] = useState(true);
 	const alertInDevelop = () => {
 		toast("In develop ... or not", { theme: "light", transition: Bounce });
 	}
@@ -16,10 +18,20 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
 		document.addEventListener("contextmenu", e => {
 			e.preventDefault();
 		});
+
+		(async () => {
+			try {
+				await fetch(process.env.NEXT_PUBLIC_API_HOST as string);
+				setIsOffline(false);
+			} catch {
+				setIsOffline(true);
+				toast("Offline Mode (Backend is not available)");
+			}
+		})();
 	}, []);
 
 	return (
-		<AppContext.Provider value={{ alertInDevelop }}>{children}</AppContext.Provider>
+		<AppContext.Provider value={{ isOffline, alertInDevelop }}>{children}</AppContext.Provider>
 	)
 }
 
